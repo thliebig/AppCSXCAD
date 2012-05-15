2b5ae2188dc118d3f090315cd08bb359a4dbc719
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2012-05-15 15:53:06 +0200

    fix: remove all library path

--- AppCSXCAD.sh
+++ AppCSXCAD.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
 DEVEL_PATH=$(dirname $0)/..
-export DEVEL_PATH
 
-#if [ -n "$LD_LIBRARY_PATH" ]; then
-#   export LD_LIBRARY_PATH=${DEVEL_PATH}/CSXCAD:${DEVEL_PATH}/QCSXCAD:${DEVEL_PATH}/tinyxml:${DEVEL_PATH}/fparser:${LD_LIBRARY_PATH}
-#else
-   export LD_LIBRARY_PATH=${DEVEL_PATH}/CSXCAD:${DEVEL_PATH}/QCSXCAD:${DEVEL_PATH}/tinyxml:${DEVEL_PATH}/fparser
-#fi
-
-echo "Setup Library path"
-echo $LD_LIBRARY_PATH
-
-echo "Run AppCSXCAD"
+export LD_LIBRARY_PATH=
 
 $DEVEL_PATH/AppCSXCAD/AppCSXCAD $@

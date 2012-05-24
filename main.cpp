7be02e76b7e25e9b3117ec66ada824684a0c6d00
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2012-05-24 10:31:07 +0200

    win64 fix

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[])
 {
-#ifndef WIN32
+#if not defined(__WIN32) && not defined(__WIN64)
 	//prevent that Qt changes float handling, e.g. expecting a german 1,345e+3 will fail...
 	setenv("LC_NUMERIC","C",1);
 #endif

af4a59e227a2986052d74385588f288a8e3bf848
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2018-12-26 22:57:36 +0100

    cmake + vtk: improve vtk libraries and better vtk>=8 support
    Signed-off-by: Thorsten Liebig <Thorsten.Liebig@gmx.de>

--- main.cpp
+++ main.cpp
@@ -17,16 +17,23 @@
 
 #include "AppCSXCAD.h"
 
-#include <QApplication>
+#include <QVTKOpenGLWidget.h>
+#if VTK_MAJOR_VERSION>=8
+  #include <QSurfaceFormat>
+  #include <QApplication>
+#endif
 
 int main(int argc, char *argv[])
 {
+#if VTK_MAJOR_VERSION>=8
+    QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
+#endif
 #if not defined(__WIN32) && not defined(__WIN64)
 	//prevent that Qt changes float handling, e.g. expecting a german 1,345e+3 will fail...
 	setenv("LC_NUMERIC","C",1);
 #endif
 	QApplication a(argc, argv);
-    AppCSXCAD w;
-    w.show();
-    return a.exec();
+	AppCSXCAD w;
+	w.show();
+	return a.exec();
 }
